120860e3b90cb96948b5fa0a58f5c056fe93a510
user.name <gmail@hdknr.com>
2020-07-25 08:37:28 +0900

    sample: Styled-Component with emotion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
 import React from "react"
+import { css } from "@emotion/core"
+import tw from "tailwind.macro"
+import styled from "@emotion/styled"
 import "../styles/style.css"
 
+const Container = styled.div`
+${[tw`text-blue-500`, css`margin:10px; padding:20px; border: solid`]}
+`
 
 const Home: React.FunctionComponent = () => {
   return (
-    <div className="text-blue-500">
+    <Container>
       Hello, World!
-    </div>
+    </Container>
   );
 }
 
