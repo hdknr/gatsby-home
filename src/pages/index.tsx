0a0c661b0afb8822763cd5643c328c5a1b1a2080
user.name <gmail@hdknr.com>
2020-07-25 08:15:11 +0900

    sample: tailwind css works

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import React from "react"
+import "../styles/style.css"
 
 
 const Home: React.FunctionComponent = () => {
   return (
-    <div>
+    <div className="text-blue-500">
       Hello, World!
     </div>
   );
