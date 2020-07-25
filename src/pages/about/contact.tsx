6aff5040052d5e100825cedaa6924d3ade09c6fb
user.name <gmail@hdknr.com>
2020-07-25 18:41:36 +0900

    About pages

---
+++ src/pages/about/contact.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { css } from "@emotion/core"
+import tw from "tailwind.macro"
+import { Layout } from '../../components/layouts';
+
+const Page: React.FunctionComponent = () => {
+  return (
+    <Layout title="Contact">
+      Contact
+    </Layout>
+  );
+}
+
+export default Page;
