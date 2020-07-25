39284e531cdcfe3767d83f1cc7858fae0f62a2d5
user.name <gmail@hdknr.com>
2020-07-25 18:29:47 +0900

    delete Theme UI / Jest sample

--- src/gatsby-plugin-theme-ui/index.ts
+++
@@ -1,9 +0,0 @@
-// See other presets to try at https://theme-ui.com/packages/presets
-import { funk } from '@theme-ui/presets'
-
-export default {
-  ...funk,
-  styles: {
-    ...funk.styles,
-  },
-}
